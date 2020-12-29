3509aaca928cf4f508eb5a10c2a232bd97334138
Sandeep Kumar Gangarapu <sandeepgangarapu@users.noreply.github.com>
2020-12-30 01:01:20 +0530

    pkl valid

--- ml_model/valid_data.py
+++ ml_model/valid_data.py
@@ -3,11 +3,11 @@ import pickle
 
 
 valid_data = pd.read_csv('all_data_for_final.csv')
-valid_data = valid_data.set_index('ID_DEMO')
 
 
 # load the model from disk
 loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
 
 a = loaded_model.predict_proba(valid_data)[:,1]
-print(a.head())
+b = pd.DataFrame({'ID_DEMO':valid_data['ID_DEMO'], 'prob': a})
+b.to_csv('valid_predict_proba.csv')
