87db323fd251b404e0fc5b99096a4095ab2f67a0
Sandeep Kumar Gangarapu <sandeepgangarapu@users.noreply.github.com>
2020-12-29 19:16:41 +0530

    changing parm grid

--- ml_model/xgboost_model.py
+++ ml_model/xgboost_model.py
@@ -66,14 +66,13 @@ pipe = Pipeline(steps=[('preprocessor', preprocessor),
 
 
 param_grid = {
- 'clf__max_depth':range(3,10,2),
- 'clf__min_child_weight':range(1,6,2)
+ 'clf__max_depth':[2,3,4],
+ 'clf__min_child_weight':[4,5,6]
 }
 
 
-pipe.steps.append(['clf', XGBClassifier(learning_rate =0.1, n_estimators=140, max_depth=5,
- min_child_weight=1, gamma=0, subsample=0.8, colsample_bytree=0.8,
- objective= 'binary:logistic', nthread=4, scale_pos_weight=1, seed=27)])
+pipe.steps.append(['clf', XGBClassifier(learning_rate=0.1, n_estimators=140, max_depth=5,
+ min_child_weight=1, gamma=0, subsample=0.8, colsample_bytree=0.8, seed=27)])
 
 
 grid = GridSearchCV(pipe, n_jobs=-1, param_grid=param_grid, scoring='roc_auc', cv=3)
